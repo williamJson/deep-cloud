13a55fa5223e5643294f7b2efa759d40e4455a68
wyqe470 <342622023@qq.com>
2021-02-21 15:27:20 +0800

    init project

---
+++ deep-account/src/main/java/com/wyq/account/AccountApplication.java
@@ -0,0 +1,19 @@
+package com.wyq.account;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 功能说明：
+ * account服务启动入口
+ *
+ * @author wyq
+ * @email 342622023@qq.com
+ * @phone 13155318100
+ */
+@SpringBootApplication
+public class AccountApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(AccountApplication.class, args);
+    }
+}
---
+++ deep-core/src/main/java/com/wyq/core/entity/BaseEntity.java
@@ -0,0 +1,11 @@
+package com.wyq.core.entity;
+
+/**
+ * 功能说明：
+ *
+ * @author wyq
+ * @email 342622023@qq.com
+ * @phone 131****8100
+ */
+public class BaseEntity {
+}
